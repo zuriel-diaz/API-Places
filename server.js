9e395a7953242f5ad02d20aa9a3eaf339c119210
xthr3mx <xthr3mx@gmail.com>
2016-09-16 15:57:19 -0500

    iteration.

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ var http        = require('http');
 var app         = express();
 
 // FB variables
-var VALIDATION_TOKEN = "EAAETMkfFTpEBAFZA7BCZBCP9BenhJvBlFSt0dWptKoP23dvTDqZCtxZBzcTteVIC83Ajjx7Ng1ZCDD31LH9SfMJvPGGysNDcdsw1zjtFCBowm1pHVOeBPrVoJsAlroetRXD9rODZCs8TWge7ZAgdJz3kbvnSZA6XBxa5JgzQCUGcqwZDZD";
+var VALIDATION_TOKEN    = "EAAETMkfFTpEBAFZA7BCZBCP9BenhJvBlFSt0dWptKoP23dvTDqZCtxZBzcTteVIC83Ajjx7Ng1ZCDD31LH9SfMJvPGGysNDcdsw1zjtFCBowm1pHVOeBPrVoJsAlroetRXD9rODZCs8TWge7ZAgdJz3kbvnSZA6XBxa5JgzQCUGcqwZDZD";
+var PAGE_ACCESS_TOKEN   = "EAAETMkfFTpEBALcVcwCEZCHZBFoQrwsv3jPZAPMYuoWtRTUGwLF1oejgnmZBDzTUV7cEv5enLaSEkguhpSASlH1J6BqCTIkoGkRbW0qkv52uIopbymhEaTkFkbqUzbhxYwuLZCyZAc3ZB6kUZBACEyQs7vZB0PjQsbyfxZA5PeesZAOVQZDZD";
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,6 +22,87 @@ app.use(bodyParser.json());
  */
 var port        = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var ip_address  = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+function receivedMessage(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
+
+    console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
+
+    var messageId = message.mid;
+
+    // You may get a text or attachment but not both
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+
+    if (messageText) {
+
+        // If we receive a text message, check to see if it matches any special
+        // keywords and send back the corresponding example. Otherwise, just echo
+        // the text we received.
+        switch (messageText) {
+          case 'image':
+            sendImageMessage(senderID);
+            break;
+
+          case 'button':
+            sendButtonMessage(senderID);
+            break;
+
+          case 'generic':
+            sendGenericMessage(senderID);
+            break;
+
+          case 'receipt':
+            sendReceiptMessage(senderID);
+            break;
+
+          default:
+            sendTextMessage(senderID, messageText);
+        }
+    } 
+    else if (messageAttachments) {
+        sendTextMessage(senderID, "Message with attachment received");
+    }
+}
+
+function sendTextMessage(recipientId, messageText) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            text: messageText
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+    request({
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: PAGE_ACCESS_TOKEN },
+        method: 'POST',
+        json: messageData
+
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
+
+            console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
+        } else {
+            console.error("Unable to send message.");
+            console.error(response);
+            console.error(error);
+        }
+    });  
+}
 
 /*
  * Routing
