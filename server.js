6e235f0b14a31fc42f94eaa88802059cf319c987
xthr3mx <xthr3mx@gmail.com>
2016-09-16 15:44:11 -0500

    Webhook implementation(POST method).

--- server.js
+++ server.js
@@ -50,7 +50,37 @@ router.route('/webhook')
         }  
     })
     .post(function(req,res){
-        res.json({ message: 'ok...' });
+        var data = req.body;
+        // Make sure this is a page subscription
+        if (data.object == 'page') {
+        // Iterate over each entry
+        // There may be multiple if batched
+        data.entry.forEach(function(pageEntry) {
+            var pageID = pageEntry.id;
+            var timeOfEvent = pageEntry.time;
+
+            // Iterate over each messaging event
+            pageEntry.messaging.forEach(function(messagingEvent) {
+                if (messagingEvent.optin) {
+                    receivedAuthentication(messagingEvent);
+                } else if (messagingEvent.message) {
+                    receivedMessage(messagingEvent);
+                } else if (messagingEvent.delivery) {
+                  receivedDeliveryConfirmation(messagingEvent);
+                } else if (messagingEvent.postback) {
+                  receivedPostback(messagingEvent);
+                } else {
+                  console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+                }
+            });
+        });
+
+        // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds, to let us know you've 
+        // successfully received the callback. Otherwise, the request will time out.
+        res.sendStatus(200);
+        }
     });
 
 // Register our routes
